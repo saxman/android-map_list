2464732114c4c5b59fc966f20ae9e41c7ddb2346
Paul R. Saxman <saxman@google.com>
2014-12-29 20:29:28 -0800

    Added default constructor to allow for subclassing

--- app/src/main/java/com/example/google/maplist/MapLocation.java
+++ app/src/main/java/com/example/google/maplist/MapLocation.java
@@ -22,6 +22,9 @@ public class MapLocation {
     public String name;
     public LatLng center;
 
+    @SuppressWarnings("unused")
+    public MapLocation() {}
+
     public MapLocation(String name, double lat, double lng) {
         this.name = name;
         this.center = new LatLng(lat, lng);
